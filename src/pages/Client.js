884ce983aa6f6e4235caa710e875617922bf6eee
Ali Klein <alexandraeklein@yahoo.com>
2020-07-15 21:08:09 -0700

    .env

--- src/pages/Client.js
+++ src/pages/Client.js
@@ -1,8 +1,8 @@
 import { createClient } from "contentful";
 
 const client = createClient({
-  space: "gi4ibdx53d45",
-  accessToken: "3TcSPkZHDbI-JnK5zgoGqzm_B7f3WnKQwvYqgwPqWLU",
+  space: process.env.REACT_APP_SPACE_ID,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 });
 
 export default client;
